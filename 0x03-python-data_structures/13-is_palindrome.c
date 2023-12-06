70f0aa374542975caf7770ccfe2b8fc981dac011
Ayoub <aoumraniayoub658@gmail.com>
2023-12-06 05:02:19 -0800

    13-is_palindrome.c done

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - function that checks if a singly linked list is a palindrome
+ * @head: The singly linked list
+ *
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+*/
+int is_palindrome(listint_t **head)
+{
+	listint_t *tmp1 = *head;
+	listint_t *tmp2 = *head;
+	int size = find_length(*head);
+	int i, j;
+
+	if (size < 2)
+		return (1);
+
+	for (i = 1; i <= size / 2; i++)
+	{
+		tmp2 = *head;
+		for (j = 0; j < size - i; j++)
+		{
+			tmp2 = tmp2->next;
+		}
+		if (tmp2->n != tmp1->n)
+			return (0);
+		tmp1 = tmp1->next;
+	}
+	return (1);
+}
