1b8b91a17f4299882e4e2f0b74a2f797f0732386
Ayoub <aoumraniayoub658@gmail.com>
2023-12-09 01:46:26 -0800

    10-best_score.py done

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        largest = max(list(my_dict.values()))
+        largest = max(list(a_dictionary.values()))
         for k in a_dictionary:
             if a_dictionary[k] == largest:
                 return k
